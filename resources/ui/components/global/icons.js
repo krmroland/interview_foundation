5d46e1b1bd7b42cf51de6bdb405a91552936eac5
krmroland <rolandmbasa@gmail.com>
2020-04-20 20:21:44 +0300

    allow users to turn on and off token visibilty

--- resources/ui/components/global/icons.js
+++ resources/ui/components/global/icons.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
-import { IconsPlugin, BIconShieldLock } from 'bootstrap-vue';
-Vue.use(IconsPlugin);
+import { BIconStar, BIconShieldLock, BIconEye, BIconEyeSlash } from 'bootstrap-vue';
+//Vue.use(IconsPlugin);
 
 Vue.component('BIconShieldLock', BIconShieldLock);
+Vue.component('BIconStar', BIconStar);
+Vue.component('BIconEye', BIconEye);
+Vue.component('BIconEyeSlash', BIconEyeSlash);
